07c1e5a4f58952ec65ab0db510352d225eadad0d
DS001\ds001 <ds001pl@outlook.com>
2022-06-29 13:27:26 +0530

    update routings

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,5 +1,5 @@
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
+import { Navigate, useNavigate, useLocation } from 'react-router-dom';
 
 import { history } from 'helper/history';
 import EviroConfig from 'config-items';
@@ -7,13 +7,12 @@ import EviroConfig from 'config-items';
 export { PrivateRoute };
 
 function PrivateRoute({ children }) {
+    history.navigate = useNavigate();
+    history.location = useLocation();   
     const { user: authUser } = useSelector(x => x.auth);
     debugger
     if (!authUser) {
-        // not logged in so redirect to login page with the return url
         return <Navigate to={ EviroConfig.path.landing.home } state={{ from: history.location }} />
     }
-
-    // authorized so return child components
     return children;
 }
--- src/routes/PublicRoute.js
+++ src/routes/PublicRoute.js
@@ -1,5 +1,5 @@
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
+import { Navigate, useNavigate, useLocation } from 'react-router-dom';
 
 import { history } from 'helper/history';
 import EviroConfig from 'config-items';
@@ -7,13 +7,12 @@ import EviroConfig from 'config-items';
 export { PublicRoute };
 
 function PublicRoute({ children }) {
+    history.navigate = useNavigate();
+    history.location = useLocation();  
     const { user: authUser } = useSelector(x => x.auth);
     debugger
-    if (authUser) {
-        // logged in so redirect to login page with the return url
+    if (authUser) {       
         return <Navigate to={ EviroConfig.path.main.dashboard } state={{ from: history.location }} />
-    }
-
-    // authorized so return child components
+    }    
     return children;
 }
